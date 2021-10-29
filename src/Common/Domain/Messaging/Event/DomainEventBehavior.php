7e96642651ca2a284c21f8ec8a51fa67888959c2
Aftab Hussain Miranda <wafto.mx@gmail.com>
2021-10-29 11:42:58 -0500

    wip

--- src/Common/Domain/Messaging/Event/DomainEventBehavior.php
+++ src/Common/Domain/Messaging/Event/DomainEventBehavior.php
@@ -6,6 +6,12 @@ namespace App\Common\Domain\Messaging\Event;
 
 trait DomainEventBehavior
 {
+    /**
+     * Return some unique string representing the event type with the format:
+     *    company.service.version.event.entity.event
+     * example:
+     *    acme.blog.1.event.post.published
+     */
     abstract public static function type(): string;
 
     /**
@@ -20,24 +26,38 @@ trait DomainEventBehavior
         protected array $payload,
         protected array $headers = [],
     ) {
-        $this->headers['__event_id'] = $id;
-        $this->headers['__event_occurred_on'] = $occurredOn;
+        $this->setId($id);
+        $this->setOcurredOn($occurredOn);
     }
 
     /**
-     * @return string
+     * A universal unique identifier string, the way to go is using UUID version 4.
      */
     public function id(): string
     {
-        return (string) $this->headers['__event_id'];
-    }
-
-    public function occuredOn(): string
-    {
-        return (string) $this->headers['__event_occurred_on'];
+        return (string) $this->headers['_event_id'];
     }
 
     /**
+     * The datetime string representation with DateTime::ATOM format.
+     */
+    public function occuredOn(): string
+    {
+        return (string) $this->headers['_event_occurred_on'];
+    }
+
+    protected function setId(string $id): void
+    {
+        $this->headers['_event_id'] = $id;
+    }
+
+    protected function setOcurredOn(string $ocurredOn): void
+    {
+        $this->headers['__event_occurred_on'] = $ocurredOn;
+    }
+
+    /**
+     * The event body.
      * @return array<string, scalar|array<scalar>|array<string, scalar>|null>
      */
     public function payload(): array
@@ -46,6 +66,7 @@ trait DomainEventBehavior
     }
 
     /**
+     * Metadata for the event like, here we store the event id, aggregate id, version, ocurred on.
      * @return array<string, scalar|array<scalar>|null>
      */
     public function headers(): array
@@ -54,6 +75,8 @@ trait DomainEventBehavior
     }
 
     /**
+     * Clone the current event and place or replace with a header and value.
+     *
      * @param string $key
      * @param scalar|array<scalar>|null $value
      * @return DomainEvent
